decb35c8f1a249e53458f1bd00eaba312f0152db
Darren Britton <darrenbritton07@gmail.com>
2022-01-17 19:36:25 +0000

    fix: name submit on enter

--- pages/login.js
+++ pages/login.js
@@ -2,7 +2,7 @@ const Login = ({ playerAdd, setName }) => (
   <div className="text-center">
     <p className="text-2xl">Enter your name</p>
     <div className="py-5">
-      <input onInput={(e) => setName(e.target.value.trim())} className="rounded-md shadow-md border-gray-50 border-2 py-2 px-4 mx-2" autoComplete="false" />
+      <input onInput={(e) => setName(e.target.value.trim())} onKeyPress={(e) => e.key === 'Enter' && playerAdd()} className="rounded-md shadow-md border-gray-50 border-2 py-2 px-4 mx-2" autoComplete="false" />
       <button
         onClick={playerAdd}
         className="rounded-md shadow-md py-2 px-4 bg-white bg-gradient-to-br text-white from-blue-400 to-blue-600 hover:from-yellow-400 hover:to-yellow-600 transition duration-75 ease-in-out"
